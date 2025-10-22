acad7b217572c84d8100b4f5f2f216013bbcf412
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-23 00:04:47 +0700

    feat: adjust interface logout

--- logout/main.go
+++ logout/main.go
@@ -5,12 +5,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Logout() {
 	fmt.Printf("\x1bc")
 	scanner := bufio.NewScanner(os.Stdin)
-	user.UserLogin = user.User{}
-	fmt.Print("Logout succes, enter to back to home.. ")
-	scanner.Scan()
+	reader := bufio.NewReader(os.Stdin)
+	loop := true
+
+	for loop {
+		fmt.Print("Are you sure you want to logout (y/n)? ")
+		inputConfirm, _ := reader.ReadString('\n')
+		confirm := strings.TrimSpace(inputConfirm)
+		if strings.ToLower(confirm) == "y" {
+			user.UserLogin = user.User{}
+			fmt.Print("Logout success, enter to back to home.. ")
+			scanner.Scan()
+			loop = false
+		} else if strings.ToLower(confirm) != "n" {
+			fmt.Print("Invalid input, please try again.. ")
+			scanner.Scan()
+		}
+	}
 }
