fb9b69b6a7826e62db5c940fe3c794e767ff36a4
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-22 17:22:28 +0700

    feat: create conditional interface in main

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"auth-flow/login"
 	"auth-flow/register"
+	"auth-flow/user"
 	"bufio"
 	"fmt"
 	"os"
@@ -17,28 +18,52 @@ func main() {
 		fmt.Printf("\x1bc")
 		fmt.Print("--- Welcome to System Authentication Flow ---\n\n")
 
-		fmt.Println("1. Register")
-		fmt.Println("2. Login")
-		fmt.Println("3. Forgot Password")
+		if user.UserLogin.Email != "" {
+			fmt.Printf("Hello %v!\n", user.UserLogin.Email)
+			fmt.Println("1. List All Users")
+			fmt.Println("2. Logout")
 
-		fmt.Print("\n0. exit\n\n")
+			fmt.Print("\n0. exit\n\n")
 
-		fmt.Print("Choose a menu: ")
-		input, _ := reader.ReadString('\n')
-		menu := strings.TrimSpace(input)
+			fmt.Print("Choose a menu: ")
+			input, _ := reader.ReadString('\n')
+			menu := strings.TrimSpace(input)
 
-		switch menu {
-		case "1":
-			register.Register()
-		case "2":
-			login.Login()
-		case "3":
-			fmt.Print("3")
-		case "0":
-			loop = false
-		default:
-			fmt.Print("error")
-			scanner.Scan()
+			switch menu {
+			case "1":
+				fmt.Print("1")
+			case "2":
+				fmt.Print("2")
+			case "0":
+				loop = false
+			default:
+				fmt.Print("error")
+				scanner.Scan()
+			}
+		} else {
+			fmt.Println("1. Register")
+			fmt.Println("2. Login")
+			fmt.Println("3. Forgot Password")
+
+			fmt.Print("\n0. exit\n\n")
+
+			fmt.Print("Choose a menu: ")
+			input, _ := reader.ReadString('\n')
+			menu := strings.TrimSpace(input)
+
+			switch menu {
+			case "1":
+				register.Register()
+			case "2":
+				login.Login()
+			case "3":
+				fmt.Print("3")
+			case "0":
+				loop = false
+			default:
+				fmt.Print("error")
+				scanner.Scan()
+			}
 		}
 	}
 }
